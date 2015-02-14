a84c8c9235b8e6a9b9329a0ac2973f2e5c3b38fd
Will Wheatley <will@iwill.id.au>
2015-02-15 10:26:49 +1100

    Line number specification

--- withspec/collector.py
+++ withspec/collector.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import logging
+import inspect
 from .registry import get_registry
 from .elements import TestElement, UnknownElement
 
@@ -45,7 +46,7 @@ class WithSpecCollector(object):
             # Try to see if there is a line number reference
             filename, line_no = location.rsplit(':', 1)
             if os.path.isfile(filename):
-                return self.collect_from_file(filename, line_no)
+                return self.collect_from_file(filename, int(line_no))
         raise ValueError, 'No such file or directory: {}'.format(location)
 
     def collect_from_dir(self, dirname):
@@ -86,10 +87,41 @@ class WithSpecCollector(object):
         # This allows the Contexts to determine what args have been
         # called, which lets them decide if a function is a fixture 
         # or a test (if not labeled as such).
+        if line is None:
+            self.resolve_and_build(file_globals.elements)
+            return
+
+        # Locate a test on that line number
         for element in file_globals.elements:
+            lines, lineno = inspect.getsourcelines(element.actual)
+            if lineno <= line <= (lineno + len(lines) - 1):
+                self.resolve_and_build([element])
+                return
+
+        # Locate a the first test after the given line and then catch
+        # all the test in that context
+        context = None
+        for element in file_globals.elements:
+            lines, lineno = inspect.getsourcelines(element.actual)
+            if lineno > line:
+                context = element.context
+                break
+        if context is None:
+            return
+        elements = []
+        for element in file_globals.elements:
+            if element.context == context:
+                elements.append(element)
+        self.resolve_and_build(elements)
+
+
+
+
+    def resolve_and_build(self, elements):
+        for element in elements:
             element.resolve_fixtures()
 
-        for element in file_globals.elements:
+        for element in elements:
             test = element.build()
             if test is not None:
                 self.tests.append(test)
